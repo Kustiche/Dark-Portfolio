e36bea273e92f8cf5aa415e7f2f5ac3344aea398
Kustiche <ChernichnyKustik@mail.ru>
2024-01-02 11:01:31 +0300

    full website adaptation

--- assets/js/functions/swiper-init.js
+++ assets/js/functions/swiper-init.js
@@ -1,15 +1,17 @@
 import Swiper from 'swiper/bundle';
 
+const screenWidth = window.screen.width;
+
 const swiper = new Swiper('.swiper', {
 	speed: 300,
 	loop: true,
-	spaceBetween: 10,
-	slidesPerView: 3,
+	spaceBetween: screenWidth <= 1024 ? 8 : 20,
+	slidesPerView: screenWidth <= 1024 ? 2 : 3,
 	centeredSlides: true,
 
-	autoplay: {
-		delay: 4000,
-	},
+	// autoplay: {
+	// 	delay: 4000,
+	// },
 
 	navigation: {
 		nextEl: '.swiper-btn--next',
