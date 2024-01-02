d28fb9bd453adb349d65317f6562b3497b59a096
Kustiche <ChernichnyKustik@mail.ru>
2024-01-02 11:29:22 +0300

    Changed the hover on the site theme switcher

--- assets/js/functions/changeTheme.js
+++ assets/js/functions/changeTheme.js
@@ -1,5 +1,20 @@
 const themeBtn = document.querySelector('.sidebar__switch');
+const namesTheme = document.querySelectorAll('.sidebar__text');
+
+function changeTheme() {
+	document.body.classList.toggle('light-theme');
+
+	namesTheme.forEach((name) => {
+		const isNameActive = name.className === 'sidebar__text sidebar__text--active';
+
+		if (isNameActive) {
+			name.classList.remove('sidebar__text--active');
+		} else {
+			name.classList.add('sidebar__text--active');
+		}
+	});
+}
 
 themeBtn.addEventListener('click', () => {
-	document.body.classList.toggle('light-theme');
+	changeTheme();
 });
